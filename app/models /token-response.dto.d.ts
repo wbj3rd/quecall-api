179a392819ef9679c7b2a2fb6ea8ebbc7e1fc882
wjohnson-softwarescientists <walter@softwarescientists.com>
2022-12-09 14:44:35 -0500

    fresh

--- app/models /token-response.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from '@loopback/repository';
-export declare class TokenResponse extends Model {
-    accessToken: string;
-    refreshToken: string;
-    constructor(data?: Partial<TokenResponse>);
-}
--- app/repositories/form.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { PostgresDataSource } from '../datasources';
-import { Form, FormRelations } from '../models';
-export declare class FormRepository extends DefaultCrudRepository<Form, typeof Form.prototype.id, FormRelations> {
-    constructor(dataSource: PostgresDataSource);
-}
